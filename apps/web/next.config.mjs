cf44b0e9f38a521c38378dcf05ad23be58cdcfc5
borgesme <jdzhao.me@gmail.com>
2026-05-12 23:02:25 +0800

    feat(web): web应用添加静态导出

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 export default {
+  output: 'export',
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: 'dist',
   experimental: { typedRoutes: false },
   transpilePackages: ['@app/shared'],
 };
